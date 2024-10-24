e89759cdee2a7109548e46ac805ae388730b85a9
Coded-Adil <itsadil2022@gmail.com>
2024-10-24 22:34:42 +0600

    Adding onClick handler to blog bookmark btn and consoleing a line

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
-const Blog = ({ blog }) => {
+import { CiBookmark } from "react-icons/ci";
+const Blog = ({ blog, handleAddToBookmark }) => {
     const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
     return (
-        <div>
-            <img src={cover} alt="" />
+        <div className='mb-20'>
+            <img className='md:w-full md:h-96 mb-8 rounded-xl' src={cover} alt="" />
             <div className='flex justify-between'>
-                <div className="flex">
+                <div className="flex mb-4">
                     <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
                     <div className='ml-6'>
                         <h3 className='text-xl'>{author}</h3>
@@ -14,12 +15,13 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{reading_time} min read</span>
+                    <button onClick={handleAddToBookmark} className='ml-2 text-xl'><CiBookmark /></button>
                 </div>
             </div>
-            <h2 className="text-4xl">{title}</h2>
+            <h2 className="text-4xl mb-6 font-bold">{title}</h2>
             <p>
                 {
-                    hashtags.map((hash, idx) => <span key={idx}><a>#{hash}</a></span>)
+                    hashtags.map((hash, idx) => <span className='m-2' key={idx}><a>{hash}</a></span>)
                 }
             </p>
         </div>
