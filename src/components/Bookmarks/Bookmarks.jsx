2db7a6f9306db544ed1b87491a6820bb244d6c01
Coded-Adil <itsadil2022@gmail.com>
2024-10-30 21:36:38 +0600

    finished this web app

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import Bookmark from '../Bookmark/Bookmark';
 const Bookmarks = ({ bookmarks, readingTime }) => {
     return (
-        <div className='md:w-1/3 mt-8'>
+        <div className='md:w-1/3 my-8'>
             <h2 className='text-3xl rounded-lg font-semibold text-center py-4 px-5 bg-purple-300 border-2 border-purple-800 my-6'>Spent time on read: {readingTime} min</h2>
             <div className="bg-gray-200 rounded-lg">
                 <h2 className="text-2xl text-center font-semibold px-5 py-4">Bookmarked Blogs: {bookmarks.length}</h2>
